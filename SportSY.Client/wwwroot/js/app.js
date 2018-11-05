77d1f1fdd400fdb8f4148590d27e775c2dce9530
ahmad <ahmad@AHMAD-DELL>
2018-11-06 00:04:21 +0200

    Add project files.

---
+++ SportSY.Client/wwwroot/js/app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("sportSy", ["ngRoute"]);
+app.config(['$routeProvider',function ($routeProvider) {
+    $routeProvider
+        .when("/users",
+        {
+            templateUrl: "/pages/users.html",
+            controller:"usersMainViewModel"
+        })
+        .otherwise("/");
+   
+}]);
