7aefdd24d56ced176a8244f54cf490545244c75e
ahmad.ammar1995 <ahmad.ammar1995@hotmail.com>
2019-01-25 11:50:49 +0000

    Deleted SportSY.Data.SQL.EF6

--- SportSY.Data.SQL.EF6/Repositories/IPersonRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using SportSY.Core.Interfaces;
-using SportSY.Core.Models;
-
-namespace SportSY.Data.SQL.Repositories
-{
-    public class PersonRepository:SQLRepositoryBase<Person,Persons>,IPersonRepository
-    {
-    }
-}
