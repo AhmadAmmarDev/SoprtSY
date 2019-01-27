596133e42a1824d1150d447478a109450d79d623
AhmadAmmarDev <ahma.ammar.1995@gmail.com>
2019-01-28 01:21:25 +0200

    add navigations link

---
+++ SportSY.Client.Web/Controllers/TeamController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using SportSY.Client.Web.Models;
+
+namespace SportSY.Client.Web.Controllers
+{
+    public class TeamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
