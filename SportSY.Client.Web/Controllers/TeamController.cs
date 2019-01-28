c18719a551a407aadd6387f8625692919c8f7e75
AhmadAmmarDev <ahma.ammar.1995@gmail.com>
2019-01-28 23:51:25 +0200

    initial of persons list to create team

--- SportSY.Client.Web/Controllers/TeamController.cs
+++ SportSY.Client.Web/Controllers/TeamController.cs
@@ -6,13 +6,28 @@ using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Mvc;
 using SportSY.Client.Web.Models;
+using SportSY.Core.Interfaces;
 
 namespace SportSY.Client.Web.Controllers
 {
     public class TeamController : Controller
     {
+        private IPersonRepository _personRepository { get; set; }
+
+        public TeamController(
+            IPersonRepository personRepository )
+        {
+            _personRepository = personRepository;
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            var persons = _personRepository.GetItems();
+            ViewBag.Persons = persons;
             return View();
         }
     }
