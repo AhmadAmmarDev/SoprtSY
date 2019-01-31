d69d89ebf62f4776f55540422b01c2fffe018a8f
AhmadAmmarDev <ahma.ammar.1995@gmail.com>
2019-01-31 19:34:50 +0200

    edit team page

--- SportSY.Client.Web/wwwroot/js/Team.js
+++ SportSY.Client.Web/wwwroot/js/Team.js
@@ -1,6 +1,17 @@
-﻿$("#playersList li").click(function (data) {
-    var perosnName = data;
+﻿var _selectedPersons = [];
+var _selectedPersonId;
+var _tagEditor = $('#selectedTeamMembers').tagEditor({
+    initialTags: ['Hello', 'World', 'Example', 'Tags'],
+    forceLowercase: false,
+    beforeTagSave: function (field, editor, tags, tag, val) {
+        _selectedPersons.push(_selectedPersonId);
+    },
+});
+$("#playersList li").click(function (data) {
+    var perosnName = data.currentTarget.getAttribute('data-person-name');
+    _selectedPersonId = data.currentTarget.getAttribute('data-person-id');
+
     $('#selectedTeamMembers').tagEditor('addTag', perosnName);
 
 });
-$('#selectedTeamMembers').tagEditor();
+
