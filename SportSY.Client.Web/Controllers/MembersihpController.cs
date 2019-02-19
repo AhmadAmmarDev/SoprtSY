9653e963be7a73a890ca935e29a55d2a6b3c38de
AhmadAmmarDev <ahma.ammar.1995@gmail.com>
2019-02-19 02:25:42 +0200

    Match request list

--- SportSY.Client.Web/Controllers/MembersihpController.cs
+++ SportSY.Client.Web/Controllers/MembersihpController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SportSY.Client.Web.Models;
+using SportSY.Client.Web.ViewModels;
 using SportSY.Core.Interfaces;
 using SportSY.Core.Models;
 using System;
@@ -10,18 +11,29 @@ using System.Threading.Tasks;
 
 namespace SportSY.Client.Web.Controllers
 {
+
     public class MembershipController : Controller
     {
         private UserManager<ApplicationUser> _userManager;
 
         private ITeamRepository _teamRepository { get; set; }
 
+        private IRequestRepository _requestRepository;
+        private IActivityRepository _activityRepository;
+        private IPlaceRepository _placesRepository;
+
         public MembershipController(
             ITeamRepository teamRepository,
+            IRequestRepository requestRepository,
+            IActivityRepository activityRepository,
+            IPlaceRepository placeRepository,
             UserManager<ApplicationUser> userManager)
         {
             _userManager = userManager;
             _teamRepository = teamRepository;
+            _requestRepository = requestRepository;
+            _activityRepository = activityRepository;
+            _placesRepository = placeRepository;
         }
         public ActionResult Index()
         {
@@ -37,6 +49,23 @@ namespace SportSY.Client.Web.Controllers
                 }
             }
             ViewBag.TeamRequstsList = teamsRequestList;
+
+            var requests = _requestRepository.GetItems();
+            var places = _placesRepository.GetItems();
+            var activities = _activityRepository.GetItems();
+
+            MatchViewModel vm = new MatchViewModel();
+            List<MatchViewModel> matchesRequest = new List<MatchViewModel>();
+            foreach (var request in requests)
+            {
+                vm.AcitivityName = activities.FirstOrDefault(e => e.ID == request.ActivityId).ArabicName;
+                vm.PlaceName = places.FirstOrDefault(e => e.ID == request.PlaceId).ArabicName;
+                vm.MatchDate = request.MatchDate;
+                vm.FromTeam = teamsLit.FirstOrDefault(e => e.ID == request.FromTeam).ArabicName;
+                vm.ToTeam = teamsLit.FirstOrDefault(e => e.ID == request.ToTeam).ArabicName;
+                matchesRequest.Add(vm);
+            }
+            ViewBag.MatchesRequests = matchesRequest;
             return View(ViewBag);
         }
 
@@ -49,12 +78,19 @@ namespace SportSY.Client.Web.Controllers
                 Guid teamID = Guid.Parse(teamId);
                 _teamRepository.AcceptTeamMember(teamID, loggedInUser.PersonId);
             }
-            else if(status == 0)
+            else if (status == 0)
             {
                 Guid teamID = Guid.Parse(teamId);
                 _teamRepository.RejectTeamMember(teamID, loggedInUser.PersonId);
 
             }
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        public ActionResult SubmitMatch(int status, string teamId)
+        {
             return RedirectToAction("Index");
         }
     }
---
+++ SportSY.Client.Web/ViewModels/MatchViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportSY.Client.Web.ViewModels
+{
+    public class MatchViewModel
+    {
+        public string PlaceName { get; set; }
+        public string AcitivityName { get; set; }
+        public string FromTeam { get; set; }
+        public string ToTeam { get; set; }
+        public DateTime? MatchDate { get; set; }
+    }
+}
